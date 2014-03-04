35b96914da36cb325decb5eee4b43bc109960f4a
Giuseppe Lipari <giuseppe.lipari@lsv.ens-cachan.fr>
2014-03-04 16:00:01 +0100

    Adding a test on the ppl (to see if it is working properly)

---
+++ tests/test_ppl_coefficient.cpp
@@ -0,0 +1,15 @@
+#include "catch.hpp"
+
+#include <syntax_trees.hpp>
+
+using namespace Parma_Polyhedra_Library::IO_Operators;
+
+TEST_CASE("Testing ppl coefficients", "[ppl]")
+{
+    Variable x(0);
+    Constraint c ( (2 * x < 0) );
+    Linear_Expression e;
+    e += c.coefficient(x) * x; 
+    cout << "Constraint: " << c << endl;
+    cout << "Linear Expression: " << e << endl;
+}
