4a82f4280e213eed2bffecf9fa3be62734d2412c
Giuseppe Lipari <giuseppe.lipari@lsv.ens-cachan.fr>
2014-03-10 13:19:20 +0100

    Resolved a little bug in automaton.cpp Added automaton.cpp in Makefile.am

--- src/automaton.cpp
+++ src/automaton.cpp
@@ -3,19 +3,19 @@
 
 rule prepare_edge_rule(edge_builder & e_builder)
 {
-  rule r_edge, r_when, r_constraint, r_ass, r_do, r_goto, r_dest;
-  r_when = rule("when", true);
-  r_do= rule("do", true);
-  r_goto= rule("goto", true);
-  r_dest = rule(tk_ident);
-  r_edge = r_when >> r_constraint >> r_do >> rule('{') >> r_ass >> *(rule(',') >> r_ass) >> rule('}') >> r_goto >> r_dest;
-  r_ass = prepare_assignment_rule(e_builder);
-  r_constraint = prepare_constraint_rule(e_builder.c_builder);
+    rule r_edge, r_when, r_constraint, r_ass, r_do, r_goto, r_dest;
+    r_when = rule("when", true);
+    r_do= rule("do", true);
+    r_goto= rule("goto", true);
+    r_dest = rule(tk_ident);
+    r_edge = r_when >> r_constraint >> r_do >> rule('{') >> r_ass >> *(rule(',') >> r_ass) >> rule('}') >> r_goto >> r_dest;
+    r_ass = prepare_assignment_rule(e_builder);
+    r_constraint = prepare_constraint_rule(e_builder.c_builder);
 
-  using namespace placeholders;
-  r_constraint [bind(&edge_builder::the_constraint, &e_builder, _1)];
-  r_dest [bind(&edge_builder::the_dest, &e_builder, _1)];
-  return r_edge;
+    using namespace placeholders;
+    r_constraint [bind(&edge_builder::the_constraint, &e_builder, _1)];
+    r_dest [bind(&edge_builder::the_dest, &e_builder, _1)];
+    return r_edge;
 }
 
 EDGE build_an_edge(const string &expr_input)
@@ -37,10 +37,6 @@ EDGE build_an_edge(const string &expr_input)
 	cout << "Parse exception!" << endl;
     }
 
-    if (!f) {
-	cout << pc.get_formatted_err_msg();
-    } else {
-	return e_builder.get_edge();
-    }
-
+    if (!f) throw pc.get_formatted_err_msg();
+    else return e_builder.get_edge();
 }
