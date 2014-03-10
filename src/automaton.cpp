cba1910dc5d59d9ee888be6d41537b63400c5ca1
Giuseppe Lipari <giuseppe.lipari@lsv.ens-cachan.fr>
2014-03-10 14:03:32 +0100

    Moved code from automaton.hpp to automaton.cpp

--- src/automaton.cpp
+++ src/automaton.cpp
@@ -1,9 +1,44 @@
 #include "automaton.hpp"
 
+using namespace tipa;
+using namespace std;
+
+
+EDGE edge_builder::get_edge() 
+{
+    return e;
+}
+
+void edge_builder::an_assignment(parser_context &pc)
+{
+    e.assignments.push_back(ass_builder.get_assignment());
+}
+
+void edge_builder::the_constraint(parser_context &pc)
+{
+    e.constraint = c_builder.get_tree();
+}
+
+void edge_builder::the_dest(parser_context &pc)
+{
+    auto x = pc.collect_tokens();
+    if (x.size() < 1) throw string("Error in collecting variable."); 
+    string v = x[x.size()-1].second;
+    e.dest = v;
+}
+
+rule prepare_assignment_rule(edge_builder &e_builder)
+{
+    rule ass = prepare_assignment_rule(e_builder.ass_builder);
+    using namespace placeholders;
+    ass [bind(&edge_builder::an_assignment, &e_builder, _1)];
+    return ass;
+}
 
 rule prepare_edge_rule(edge_builder & e_builder)
 {
     rule r_edge, r_when, r_constraint, r_ass, r_do, r_goto, r_dest;
+
     r_when = rule("when", true);
     r_do= rule("do", true);
     r_goto= rule("goto", true);
