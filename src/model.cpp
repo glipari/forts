27918ca4e4668a99503765d4972b00ccd78a89cf
Youcheng Sun <techieyoucheng@gmail.com>
2014-03-20 10:11:14 +0100

    An improved combine function.

--- src/model.cpp
+++ src/model.cpp
@@ -25,14 +25,26 @@ void model::continuous_step(sstate &ss)
     // 1) To do time_elapse_assign
     PPL::C_Polyhedron rates_cvx(cvars.size());
     PPL::C_Polyhedron invariant_cvx(cvars.size());
+    CVList lvars = cvars;
     for ( auto it = automata.begin(); it != automata.end(); it++){
 	Linear_Constraint lc;
 	string ln = ss.loc_names[it-automata.begin()];
 	location &l = it->get_location(ln);
 
 	invariant_cvx.add_constraints(l.invariant_to_Linear_Constraint(cvars, dvars));
-	rates_cvx.add_constraints(l.rates_to_Linear_Constraint(cvars, dvars));
+	rates_cvx.add_constraints(l.rates_to_Linear_Constraint(cvars, dvars, lvars));
+        //cout << "rates : " << rates_cvx << endl;
+        //cout << "invariant : " << invariant_cvx << endl;
     
+    }
+
+    // If a variable's rate is not specified in the location, it's assumed to be 1
+    for ( auto it = lvars.begin(); it != lvars.end(); it++) {
+      PPL::Variable v = get_variable(it->name, cvars);
+      Linear_Expr le;
+      le += 1;
+      AT_Constraint atc = (v==le);
+      rates_cvx.add_constraint(atc);
     }
     ss.cvx.time_elapse_assign(rates_cvx);
     // 2) intersect with invariant
@@ -58,7 +70,6 @@ void model::discrete_step(sstate &ss, const vector<edge> &edges)
     ss.cvx.intersection_assign(ass_cvx);
     ss.cvx.intersection_assign(get_invariant_cvx(ss));
 }
-
 
 template<class C, class X>
 bool contains(const C &c, const X &x) 
@@ -89,10 +100,13 @@ void combine(const location &l,
 			      new_labels.end(),
 			      std::back_inserter(common_labels));
 
+        vector< vector<edge> > origin_v_edges = v_edges;
+        //v_edges.clear();
+        vector< vector<edge> > sub_v_edges;
 	for (auto &e : l.outgoings) { 
 	    // edge is the current outgoing edge in location l
-	    if (e.sync_label == "" or 
-		not contains(common_labels, e.sync_label)) {
+	    if (e.sync_label == "") {// or 
+		//not contains(common_labels, e.sync_label)) {
 		// no synchronisation, 
 		// the number of elements in v_edge is duplicated: 
 		// we need to add one element to each vector: it can be 
@@ -100,30 +114,71 @@ void combine(const location &l,
 		// 
 		// plus we add a new vector that contains all 
 		// stuttering except the edge element
-                for ( auto &edge_group : v_edges) {
+                for ( auto &edge_group : origin_v_edges) {
                   // duplicate already existing edge groups
-                  vector<edge> duplicated_edge_group = edge_group;
-                  v_edges.push_back(duplicated_edge_group);
+                  //vector<edge> duplicated_edge_group = edge_group;
+                  //sub_v_edges.push_back(duplicated_edge_group);
                   // append e to already existing edge groups
-                  edge_group.push_back(e);
+                  vector<edge> eg = edge_group;
+                  eg.push_back(e);
+                  sub_v_edges.push_back(eg);
                   // a new edge group with only "e" inside
                   vector<edge> new_edge_group = {e};
-                  v_edges.push_back(new_edge_group);
+                  sub_v_edges.push_back(new_edge_group);
                 }
-	    } else {
-		for (auto &edge_group : v_edges) {
+	    } else if ( not contains(common_labels, e.sync_label)) {
+              // there is synchronisation,
+              // and it does not synchronize with any previously visited edge
+              vector<edge> new_edge_group = {e};
+              sub_v_edges.push_back(new_edge_group);
+              // "e" can be appended to an edge group such that all edges inside it have empty synch label ""
+              for (auto &edge_group : origin_v_edges) {
+                bool empty_sync = true;
+                for (auto &e1 : edge_group)
+                  if (e1.sync_label != "") {
+                    empty_sync = false;
+                    break;
+                  }
+                if (empty_sync) {
+                  vector<edge> eg = edge_group;
+                  eg.push_back(e);
+                  sub_v_edges.push_back(eg);
+                }
+              }
+            }
+            else {
+		for (auto &edge_group : origin_v_edges) {
 		    for (auto existing_edge : edge_group) {
 			if (e.sync_label == existing_edge.sync_label) {
 			    // yes they synchronise
 			    // Add this only to the corresponding edge_group
 			    // and skip the rest
-                            edge_group.push_back(e);
+                            //edge_group.push_back(e);
+                            vector<edge> eg = edge_group;
+                            eg.push_back(e);
+                            sub_v_edges.push_back(eg);
+                            // remove the half synchronized edge group in v_edges
+                            auto xit = v_edges.begin();
+                            while (xit != v_edges.end()) {
+                              bool existing = false;
+                              for ( auto &xxit : *xit) {
+                                if (xxit.sync_label == existing_edge.sync_label)
+                                  existing = true;
+                                if (existing) break;
+                              }
+                              if (existing) v_edges.erase(xit);
+                              else xit ++;
+                            }
+                            break;
 			}
 			// otherwise, do nothing
 		    }
 		}
 	    }
 	}
+        // store all new combinations into v_edges
+        for (auto &xx : sub_v_edges)
+          v_edges.push_back(xx);
     }
     vector<string> result;
     std::set_union(synch_labels.begin(), 
@@ -134,6 +189,7 @@ void combine(const location &l,
     synch_labels = result;
 }
 
+
 vector<sstate> model::Post(const sstate& ss)
 {
     vector< vector<edge> > v_edges;
@@ -141,56 +197,41 @@ vector<sstate> model::Post(const sstate& ss)
     vector<sstate> &sstates = v_ss;
     vector<string> synch_labels; 
 
-    int a_index = 0;
+    int a_index;
+    vector< vector<edge> > edge_groups;
     for (auto loc_it = ss.loc_names.begin(); loc_it != ss.loc_names.end(); ++loc_it, ++a_index) {
+        a_index = loc_it - ss.loc_names.begin();
 	location &l = automata[a_index].get_location(*loc_it);
-	/** To browse each outgoing edge from location "l". */
-	// for (auto iit = l.outgoings.begin(); iit != l.outgoings.end(); iit++) {
-	//     if (a_index == 0) {
-	// 	vector<edge> v;
-	// 	v.push_back(*iit);
-	// 	v_edges.push_back(v);
-	// 	continue;
-	//     }
-	//     for ( auto ev = v_edges.begin(); ev != v_edges.end(); ev++) {
-	// 	if ( ev->size() <= it - ss.loc_names.begin())
-	// 	    continue;
-	// 	bool combination = true;
-	// 	for ( auto eit = ev->begin(); eit != ev->end(); eit++) {
-	// 	    if (eit->sync_label!="" && iit->sync_label!="" && eit->sync_label!=iit->sync_label) {
-	// 		combination = false;
-	// 		break;
-	// 	    }
-	// 	}
-	// 	if (combination) ev->push_back(*iit);
-	//     }
-	// }
-	
+        vector<string> new_labels = automata[a_index].labels;
+        combine(l, new_labels, edge_groups, synch_labels, loc_it==ss.loc_names.begin());
     }
+        cout << "-----------------------------"<< endl;
+        cout << "Start from : "; 
+        for ( auto &y : ss.loc_names)
+          cout << y ;
+        cout << endl;
+        for ( auto it = edge_groups.begin(); it != edge_groups.end(); it++){
+          for ( auto iit = it->begin(); iit != it->end(); iit++)
+            iit->print();
+          cout << endl;
+        }
+          
+        cout << "-----------------------------"<< endl;
 
-    for ( auto it = v_edges.begin(); it != v_edges.end(); it++) {
-	if (it->size() != ss.loc_names.size())
-	    continue;
-	//bool repetition = true;
-	//for (auto iit = it->begin(); iit != it->end(); iit++) {
-	//  if ( iit->sync_label != "") {
-	//    repetition = false;
-	//    break;
-	//  }
-	//  if ( iit->dest != ss.loc_names[iit-it->begin()]) {
-	//    repetition = false;
-	//    break;
-	//  }
-	//  PPL::C_Polyhedron c(iit->guard_to_Linear_Constraint(cvars, dvars));
-	//  if (!c.is_universe()) {
-	//    repetition = false;
-	//    break;
-	//  }
-	//}
-	//if (repetition) continue;
+    for ( auto it = edge_groups.begin(); it != edge_groups.end(); it++) {
 	sstate nss = ss;
 	discrete_step(nss, *it);
+        //cout <<"after a discrete step : " << endl;
+        //for ( auto &l : nss.loc_names)
+        //  cout<<l;
+        //cout << endl;
+        //cout << nss.cvx << endl;
 	continuous_step(nss);
+        //cout <<"after a continuous step : " << endl;
+        //for ( auto &l : nss.loc_names)
+        //  cout<<l;
+        //cout << endl;
+        //cout << nss.cvx << endl;
 	sstates.push_back(nss);
     }
 
@@ -212,6 +253,7 @@ sstate model::init_sstate()
     init.cvx = C_Polyhedron(init_constraint->to_Linear_Constraint(cvars, dvars));
     cout << "cvx : " << init.cvx << endl;
     continuous_step(init);
+    cout << "cvx after continuous step : " << init.cvx << endl;
     return init;
 }
 
@@ -232,6 +274,10 @@ void model::SpaceExplorer()
 		if ( iit->cvx.is_empty()) continue;
 		if ( is_bad(*iit))
 		    throw ("A bad location is reached ... ");
+        for ( auto &l : iit->loc_names)
+          cout<<l;
+        cout << endl;
+        cout << iit->cvx << endl;
 		if ( contained_in(*iit, current)) continue;
 		if ( contained_in(*iit, next)) continue;
 		if ( contained_in(*iit, Space)) continue;
@@ -339,3 +385,101 @@ void model::check_consistency()
     }
 }
 
+//void combine(const location &l, 
+//	     const vector<string> new_labels,
+//	     vector< vector<edge> > &v_edges, 
+//	     vector<string> &synch_labels,
+//	     bool first) 
+//{
+//    if (first) {
+//	for (auto iit = l.outgoings.begin(); iit != l.outgoings.end(); iit++) {
+//	    vector<edge> v;
+//	    v.push_back(*iit);
+//	    v_edges.push_back(v);
+//	}
+//	synch_labels = new_labels;
+//	return;
+//    } else {
+//	// this is the intersection of the two sets of synch labels
+//	vector<string> common_labels;
+//	std::set_intersection(synch_labels.begin(), 
+//			      synch_labels.end(), 
+//			      new_labels.begin(),
+//			      new_labels.end(),
+//			      std::back_inserter(common_labels));
+//
+//        vector< vector<edge> > origin_v_edges = v_edges;
+//        v_edges.clear();
+//	for (auto &e : l.outgoings) { 
+//            vector< vector<edge> > sub_v_edges;
+//	    // edge is the current outgoing edge in location l
+//	    if (e.sync_label == "") {// or 
+//		//not contains(common_labels, e.sync_label)) {
+//		// no synchronisation, 
+//		// the number of elements in v_edge is duplicated: 
+//		// we need to add one element to each vector: it can be 
+//		// a stuttering or one containing the edge
+//		// 
+//		// plus we add a new vector that contains all 
+//		// stuttering except the edge element
+//                for ( auto &edge_group : origin_v_edges) {
+//                  // duplicate already existing edge groups
+//                  vector<edge> duplicated_edge_group = edge_group;
+//                  sub_v_edges.push_back(duplicated_edge_group);
+//                  // append e to already existing edge groups
+//                  vector<edge> eg = edge_group;
+//                  eg.push_back(e);
+//                  sub_v_edges.push_back(eg);
+//                  // a new edge group with only "e" inside
+//                  vector<edge> new_edge_group = {e};
+//                  sub_v_edges.push_back(new_edge_group);
+//                }
+//	    } else if ( not contains(common_labels, e.sync_label)) {
+//              // there is synchronisation,
+//              // and it does not synchronize with any previously visited edge
+//              vector<edge> new_edge_group = {e};
+//              sub_v_edges.push_back(new_edge_group);
+//              // "e" can be appended to an edge group such that all edges inside it have empty synch label ""
+//              for (auto &edge_group : origin_v_edges) {
+//                bool empty_sync = true;
+//                for (auto &e1 : edge_group)
+//                  if (e1.sync_label != "") {
+//                    empty_sync = false;
+//                    break;
+//                  }
+//                if (empty_sync) {
+//                  vector<edge> eg = edge_group;
+//                  eg.push_back(e);
+//                  sub_v_edges.push_back(eg);
+//                }
+//              }
+//            }
+//            else {
+//		for (auto &edge_group : origin_v_edges) {
+//		    for (auto existing_edge : edge_group) {
+//			if (e.sync_label == existing_edge.sync_label) {
+//			    // yes they synchronise
+//			    // Add this only to the corresponding edge_group
+//			    // and skip the rest
+//                            //edge_group.push_back(e);
+//                            vector<edge> eg = edge_group;
+//                            eg.push_back(e);
+//                            sub_v_edges.push_back(eg);
+//                            break;
+//			}
+//			// otherwise, do nothing
+//		    }
+//		}
+//	    }
+//            for (auto &xx : sub_v_edges)
+//              v_edges.push_back(xx);
+//	}
+//    }
+//    vector<string> result;
+//    std::set_union(synch_labels.begin(), 
+//		   synch_labels.end(), 
+//		   new_labels.begin(),
+//		   new_labels.end(),
+//		   std::back_inserter(result));
+//    synch_labels = result;
+//}
