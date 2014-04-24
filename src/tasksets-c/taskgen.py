41dc316f0ca7eefcacf2fd92513815114c8c93a2
syc <syc@syc-P5Q-PRO.(none)>
2014-04-24 20:13:09 +0200

    Created the branch for schedulability check experiment.

--- src/tasksets-c/taskgen.py
+++ src/tasksets-c/taskgen.py
@@ -265,10 +265,12 @@ def main():
     options.format = options.format.replace("\\n", "\n")
 
     n = options.n
-    m = 4
+    m = 2
     seed = 12345*n*m + m + n
-    nSets = 1000
-    utils = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+    nSets = 5000
+    #utils = [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40]
+    #utils = [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+    utils = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
     numpy.random.seed(seed)
     for xx, yy in enumerate(utils) :
       options.util = yy/10.0
