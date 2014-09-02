61681c41123e57d07579aae1db7e68d8ccae64e8
syc <techieyoucheng@gmail.com>
2014-09-02 18:42:56 +0200

    Refined the main.cpp file

--- src/main.cpp
+++ src/main.cpp
@@ -12,119 +12,121 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    bool ce = false, beep = false, visual = false, on_the_fly = false;
-    int bound=-1;
-    string visual_fname;
-    if (argc == 1) {
-	cout << "Usage: " << argv[0] << " [options] <filename> " << endl;
-        return 0;
-    }
+  /**
+   * ce : the counter example guided approach
+   * visual : to visulise the result by outputing graph
+   **/
+  bool ce = false, beep = false, visual = false, on_the_fly = false;
+  /** "bound" specifies the maximum step for the analysis */
+  int bound=-1;
+  /** the "graph" name for plotting */
+  string visual_fname;
+
+  if (argc == 1) {
+    cout << "Usage: " << argv[0] << " [options] <filename> " << endl;
+    return 0;
+  }
     
-    MODEL.reset();
-    int c = -1;
-    int parall = 1;
-    string state_type = "";
-    while ((c = getopt(argc, argv, "p:s:r:v:f:b:")) != -1) {
-	if (c == 'p') {
-	    cout << "Parallelism set to :";
-	    parall = atoi(optarg);
-	    cout << parall << endl;
-	}
-	if (c == 's') {
-	    state_type = string(optarg);
-	    if (state_type == "box") 
-		    MODEL.set_sstate_type(BOX_WIDENED);
-	    else if (state_type == "widened") 
-		    MODEL.set_sstate_type(WIDENED);
-	    else if (state_type == "dbm") 
-		    MODEL.set_sstate_type(DBM);
-	    else if (state_type == "oct") 
-		    MODEL.set_sstate_type(OCT);
-	    else if (state_type == "origin")
-		    MODEL.set_sstate_type(ORIGIN);
-	    else {
-		    cout << "Unknown state type : " << state_type << endl;
-		    cout << "Possible values are: origin, widened, box_widened, dbm" << endl;
-            return 0;
-	    }
-	    cout << "State set to :" << state_type << endl;
-	}
+  MODEL.reset();
+    
+  int c = -1;
+  int parall = 1;
+  string state_type = "";
+  while ((c = getopt(argc, argv, "p:s:r:v:f:b:")) != -1) {	
+    /**
+     * p: to run the software in parallelism, which never works ... 
+     **/
+    if (c == 'p') {
+      cout << "Parallelism set to :";
+      parall = atoi(optarg);
+      cout << parall << endl;
+    }
+    /** there is a series of state types */
+    if (c == 's') {
+      state_type = string(optarg);
+      if (state_type == "box") 
+        MODEL.set_sstate_type(BOX_WIDENED);
+      else if (state_type == "widened") 
+        MODEL.set_sstate_type(WIDENED);
+      else if (state_type == "dbm") 
+	MODEL.set_sstate_type(DBM);
+      else if (state_type == "oct") 
+	MODEL.set_sstate_type(OCT);
+      else if (state_type == "origin")
+	MODEL.set_sstate_type(ORIGIN);
+      else {
+	cout << "Unknown state type : " << state_type << endl;
+	cout << "Possible values are: origin, widened, box_widened, dbm" << endl;
+        return 0;
+      }
+      cout << "State set to :" << state_type << endl;
+    }
+    /** to set up the bound on steps */
     if ( c == 'b') {
         bound = atoi(optarg);
     }
-	if (c == 'r') {
-	    string robustness_type = string(optarg);
-	    if (robustness_type == "beep") {
-            beep = true;
-        }
-	    else if (robustness_type == "ce") {
-            ce = true;
-        }
-        else {
-            cout << "Unknown type for robustness analysis : " << robustness_type << endl;
-            cout << "Possible values are : beep" << endl;
-            return 0;
-        }
+    /** the robustness analysis for the automaton model with parameters */
+    if (c == 'r') {
+      string robustness_type = string(optarg);
+      if (robustness_type == "beep") {
+        beep = true;
+      }
+      else if (robustness_type == "ce") {
+        ce = true;
+      }
+      else {
+        cout << "Unknown type for robustness analysis : " << robustness_type << endl;
+        cout << "Possible values are : beep" << endl;
+        return 0;
+      }
     }
-	if (c == 'v') {
-        visual = true;
-	    visual_fname = string(optarg);
+    /** to graphically visulise the result */
+    if (c == 'v') {
+      visual = true;
+      visual_fname = string(optarg);
     }
-	if (c == 'f') {
+    /** is the flag "on-the-fly" meaningful now? */	
+    if (c == 'f') {
         on_the_fly = true;
 	    //visual_fname = string(optarg);
     }
-    }
+  }
 
-    /** argv[1] in the input file name. */
-    ifstream ifs(argv[optind]);
-    
-    if (not ifs.good()) {
-	cout << "Could not read " << argv[1] << endl;
-	return 0;
-    }
-
-    string str((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>()); 
-
-    build_a_model(str);
-    if ( bound != -1) {
-        MODEL.set_bound(bound);
-    }
-    try {
-        /** To print out the model? */
-        //MODEL.print();
-//        MODEL.set_sstate_type(BOX_WIDENED);
-//        MODEL.set_sstate_type(WIDENED);
-        if ( beep) {
-            //if( on_the_fly) {
-            //    MODEL.beep_set_on_the_fly();
-            //}
-            MODEL.efficient_BEEP();
-            //MODEL.BEEP();
-            if( visual) 
-                MODEL.efficient_print_points(visual_fname);
-                //MODEL.print_points(visual_fname);
-            //return 0;
-        }
-        else if (ce) {
-            cout << "Before calling CE method" << endl;
-            MODEL.CE();
-        }
-        else {
-            cout << "here : " << endl;
-            MODEL.check_consistency();
-            cout << "here2 : " << endl;
-            MODEL.SpaceExplorer();
-        }
-	// MODEL.set_concurrency(2);
-        MODEL.print_log();
-    } catch(const string &s) {
-        cout << s << endl;
-        return 1;
-    } catch(const char* s) {
-        cout << s << endl;
-        return 1;
-    }
-
+  /** argv[1] in the input file name. */
+  ifstream ifs(argv[optind]);
+  if (not ifs.good()) {
+    cout << "Could not read " << argv[1] << endl;
     return 0;
+  }
+
+  string str((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>()); 
+  build_a_model(str);
+    
+  if ( bound != -1) {
+    MODEL.set_bound(bound);
+  }
+
+  try {
+    if ( beep) {
+      MODEL.efficient_BEEP();
+      if( visual) 
+        MODEL.efficient_print_points(visual_fname);
+    }
+    else if (ce) {
+      MODEL.CE();
+    }
+    else {
+      MODEL.check_consistency();
+      MODEL.SpaceExplorer();
+    }
+    MODEL.print_log();
+  } catch(const string &s) {
+    cout << s << endl;
+    return 1;
+  } catch(const char* s) {
+    cout << s << endl;
+    return 1;
+  }
+
+  return 0;
 }
